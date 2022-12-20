463c7be697f3a1151ded887607b9d97dfeb20054
Redox <mail@redox.to>
2022-12-20 22:56:46 +0100

    Remove timings and add percentage

--- lua/nitrous/utils/testlib.lua
+++ lua/nitrous/utils/testlib.lua
@@ -21,41 +21,46 @@ function GMN.TestCompare( func1, func2, count )
 
     print( "[GMN] Starting test.." )
 
-    timer.Simple( 0, function()
+    jit.on()
+    jit.flush()
+    collectgarbage()
+    local totalTime1, averageTime1 = GMN.RunTest( func1, count )
+
+    jit.on()
+    jit.flush()
+    collectgarbage()
+    local totalTime2, averageTime2 = GMN.RunTest( func2, count )
+
+    jit.off()
+    jit.flush()
+    collectgarbage()
+    local totalTime3, averageTime3 = GMN.RunTest( func1, count )
+
+    jit.off()
+    jit.flush()
+    collectgarbage()
+    local totalTime4, averageTime4 = GMN.RunTest( func2, count )
+
+    if jitstatus then
         jit.on()
-        jit.flush()
-        collectgarbage()
-        local totalTime1, averageTime1 = GMN.RunTest( func1, count )
-        print( "1 JIT ON took " .. totalTime1 .. " seconds to run " .. count .. " times, average: " .. averageTime1 .. " seconds" )
-    end )
-
-    timer.Simple( 0.05, function()
-        jit.on()
-        jit.flush()
-        collectgarbage()
-        local totalTime2, averageTime2 = GMN.RunTest( func2, count )
-        print( "2 JIT ON took " .. totalTime2 .. " seconds to run " .. count .. " times, average: " .. averageTime2 .. " seconds" )
-    end )
-
-    timer.Simple( 0.1, function()
+    else
         jit.off()
-        jit.flush()
-        collectgarbage()
-        local totalTime3, averageTime3 = GMN.RunTest( func1, count )
-        print( "1 JIT OFF took " .. totalTime3 .. " seconds to run " .. count .. " times, average: " .. averageTime3 .. " seconds" )
-    end )
+    end
 
-    timer.Simple( 0.15, function()
-        jit.off()
-        jit.flush()
-        collectgarbage()
-        local totalTime4, averageTime4 = GMN.RunTest( func2, count )
-        print( "2 JIT OFF took " .. totalTime4 .. " seconds to run " .. count .. " times, average: " .. averageTime4 .. " seconds" )
+    print( "1 JIT ON took " .. totalTime1 .. " seconds to run " .. count .. " times, average: " .. averageTime1 .. " seconds" )
+    print( "2 JIT ON took " .. totalTime2 .. " seconds to run " .. count .. " times, average: " .. averageTime2 .. " seconds" )
+    print( "1 JIT OFF took " .. totalTime3 .. " seconds to run " .. count .. " times, average: " .. averageTime3 .. " seconds" )
+    print( "2 JIT OFF took " .. totalTime4 .. " seconds to run " .. count .. " times, average: " .. averageTime4 .. " seconds" )
 
-        if jitstatus then
-            jit.on()
-        else
-            jit.off()
-        end
-    end )
+    if averageTime1 > averageTime2 then
+        print( "JIT ON 2 is " .. math.Round( ( averageTime1 / averageTime2 ) * 100 - 100, 3 ) .. "% faster than test 1" )
+    else
+        print( "JIT ON 1 is " .. math.Round( ( averageTime2 / averageTime1 ) * 100 - 100, 3 ) .. "% faster than test 2" )
+    end
+
+    if averageTime3 > averageTime4 then
+        print( "JIT OFF 2 is " .. math.Round( ( averageTime3 / averageTime4 ) * 100 - 100, 3 ) .. "% faster than test 1" )
+    else
+        print( "JIT OFF 2 is " .. math.Round( ( averageTime4 / averageTime3 ) * 100 - 100, 3 ) .. "% faster than test 2" )
+    end
 end
