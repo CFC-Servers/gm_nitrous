11459d45ff319ffe63b94ac07f0cca9563a05f1f
Redox <mail@redox.to>
2023-06-05 00:19:22 +0200

    Harden against NULL calls

--- lua/nitrous/optimizations/shared/entity_getclass.lua
+++ lua/nitrous/optimizations/shared/entity_getclass.lua
@@ -4,6 +4,9 @@ meta.GMN_GetClass = GMN.StoreOriginal( "Entity:GetClass", meta.GetClass )
 local getTable = meta.GetTable
 
 function meta:GetClass()
+    if self == NULL then return self:GMN_GetClass() end
+
+    local dataTable = getTable( self )
     local cached = getTable( self ).GMN_GetClassVar
     if cached then return cached end
 
